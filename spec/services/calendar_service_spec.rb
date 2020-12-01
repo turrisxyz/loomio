92b98ab5a9b29f4cd3fcc9d9ade6e585a13fa08c
Robert Guthrie <rob@guthrie.nz>
2020-12-01 13:03:57 +1300

    cal service

---
+++ spec/services/calendar_service_spec.rb
@@ -0,0 +1,19 @@
+require 'rails_helper'
+describe 'CalendarEventService' do
+  let(:user) { create :user }
+  let(:another_user) { create :user }
+  let(:group) { create(:group) }
+  let(:single_event) { }
+
+  it 'imports an event' do
+  end
+
+  it 'imports a recurring event' do
+  end
+
+  it 'imports a recurring event with exdates' do
+  end
+
+  it 'imports a recurring event with some occurences moved' do
+  end
+end
